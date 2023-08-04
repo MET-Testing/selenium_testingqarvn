a0ec789ed799ecad660bf6b3f0a821c83741d284
Valentina Bianchi k <valentinabianchik@MacBook-de-Facundo.local>
2023-08-04 17:31:54 -0300

    me había faltado actualizar cambios en otras paginas

--- src/test/java/pages/FormPage.java
+++ src/test/java/pages/FormPage.java
@@ -1,67 +1,77 @@
 package pages;
 
+import java.util.Map;
 
 import org.openqa.selenium.By;
+import utils.FixtureUtils;
 
-public class FormPage extends BasePage { 
+public class FormPage extends BasePage {
 
-    private String name = "(//input[@id=\"wsf-1-field-54\"])";
-    private String lastname ="//input[@id=\"wsf-1-field-55\"]";
-    private String email = "(//input[@id='wsf-1-field-56'])";
-    private String phone = "(//input[@id='wsf-1-field-57'])";
-    private String adress = "//textarea[@id='wsf-1-field-58']";
+    // private String name = "(//input[@id=\"wsf-1-field-54\"])";
+    // private String lastname ="//input[@id=\"wsf-1-field-55\"]";
+    // private String email = "(//input[@id='wsf-1-field-56'])";
+    // private String phone = "(//input[@id='wsf-1-field-57'])";
+    // private String adress = "//textarea[@id='wsf-1-field-58']";
     By submit = By.id("wsf-1-field-62");
     By radioButton1 = By.xpath("//label[@id='wsf-1-label-60-row-1']");
     By radioButton2 = By.xpath("//label[@id='wsf-1-label-60-row-2']");
     By phytonButton = By.id("wsf-1-label-59-row-2");
     By phpButton = By.id("wsf-1-label-59-row-1");
     By dropdown = By.id("wsf-1-field-61");
-    
+
+    private Map<String, String> locators;
+    private Map<String, String> validData;
+    private Map<String, String> assertLocators;
 
     public FormPage() {
 
-    super(driver);
+        super(driver);
+        Map<String, Map<String, String>> tempMap = FixtureUtils.map("src/test/java/resources/validData.json");
+        Map<String, Map<String, String>> assertMap = FixtureUtils.map("src/test/java/resources/assertionsLocators.json");
+        locators = tempMap.get("locators");
+        validData = tempMap.get("values_1");
+        assertLocators = assertMap.get("locators");
+ 
     }
 
-    public void navigateToPage(){
+    public By getLocator(String key) {
+        return By.id(locators.get(key));
+    }
+
+    public void navigateToPage() {
         navigateTo("https://testingqarvn.com.es/combobox-dependiente/");
     }
+
     public void fillForm() {
         System.out.println("ejecutando fill");
 
-        write(name, "Valentina");
-        write(lastname,"Bianchi");
-        write(email, "valentinabinachik@gmail.com");
-        write(phone, "2213508669");
-        write(adress, "curuzu cuatia");
-    
+        write(locators.get("name"), validData.get("name"));
+        write(locators.get("lastname"), validData.get("lastname"));
+        write(locators.get("email"), validData.get("email"));
+        write(locators.get("phone"), validData.get("phone"));
+        write(locators.get("address"), validData.get("address"));
     }
 
     public void clickLenguage(){
-        click(phytonButton);
-        click(phpButton);
-
-    }
-    
-    public void clickRadio(){
-        click(radioButton1);
-        click(radioButton2);
+        click(getLocator("phpButton"));
+        click(getLocator("phytonButton"));
 
     }
 
-    public void selectSystem(){
+    public void clickRadio() {
+        click(getLocator("radio1"));
+        click(getLocator("radio2"));
+
+    }
+
+    public void selectSystem() {
         selectFromDropdownByVisibleText(dropdown, "Linux");
 
     }
 
-    public void clickButton(){
+    public void clickButton() {
         click(submit);
-        
+
     }
 
-    
 }
-
-
-
-
--- src/test/java/steps/StepsFormPage.java
+++ src/test/java/steps/StepsFormPage.java
@@ -1,129 +1,102 @@
 package steps;
 
+import org.openqa.selenium.By;
+
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 import pages.BasePage;
 import pages.FormPage;
+import utils.FixtureUtils;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class StepsFormPage {
+    private static Map<String, String> locators;
+    private static Map<String, String> assertLocators;
 
-FormPage test = new FormPage();
+    private static FormPage test;
 
-@Test
-@Order(value=1)
-public void navigateToPage(){
-    test.navigateToPage();
+
+    public By getLocator(String key) {
+    return By.id(locators.get(key));
+    }
+
+    @BeforeAll
+    
+    public static void setUp() {
+    Map<String, Map<String, String>> tempMap = FixtureUtils.map("src/test/java/resources/validData.json");
+    Map<String, Map<String, String>> assertMap = FixtureUtils.map("src/test/java/resources/assertionsLocators.json");
+    locators = tempMap.get("locators");
+    assertLocators = assertMap.get("locators");
+
+    test = new FormPage();
 }
 
-@Test
-@Order(value=2)
-public void fillForm() {
-    test.fillForm();
-WebElement labelName = test.findElement(By.id("wsf-1-label-54"));
-WebElement inputName = test.findElement(By.id("wsf-1-field-54"));
-String labelTextName = labelName.getText();
-String inputPlaceholderName = inputName.getAttribute("placeholder");
-try{
-assertEquals(labelTextName, inputPlaceholderName);
-    } catch (AssertionError e) {
-        System.out.println("The Name elements dont match");
-    }
-WebElement labelLastname = test.findElement(By.id("wsf-1-label-55"));
-WebElement inputLastName = test.findElement(By.id("wsf-1-field-55"));
-String labelTextLastname = labelLastname.getText();
-String inputPlaceholderLastname = inputLastName.getAttribute("placeholder");
-try{
-assertEquals(labelTextLastname, inputPlaceholderLastname);
-    } catch (AssertionError a) {
-        System.out.println("The Lastname elements dont match");
-    }
-WebElement labelEmail = test.findElement(By.id("wsf-1-label-56"));
-WebElement inputEmail = test.findElement(By.id("wsf-1-field-56"));
-String labelTextEmail = labelEmail.getText();
-String inputPlaceholderEmail = inputEmail.getAttribute("placeholder");
-try {
-    assertEquals(labelTextEmail, inputPlaceholderEmail);
-    } catch (AssertionError b) {
-        System.out.println("The Email elements dont match");
-    }
-WebElement labelPhone = test.findElement(By.id("wsf-1-label-57"));
-WebElement inputPhone = test.findElement(By.id("wsf-1-field-57"));
-String labelTextPhone = labelPhone.getText();
-String inputPlaceholderPhone = inputPhone.getAttribute("placeholder");
-try {
-    assertEquals(labelTextPhone, inputPlaceholderPhone);
-    } catch (AssertionError c) {
-        System.out.println("The Phone elements dont match");
-    }
-WebElement labelAdress = test.findElement(By.id("wsf-1-label-58"));
-WebElement inputAdress = test.findElement(By.id("wsf-1-field-58"));
-String labelTextAdress = labelAdress.getText();
-String inputPlaceholderAdress = inputAdress.getAttribute("placeholder");
-try {
-    assertEquals(labelTextAdress, inputPlaceholderAdress);
-    } catch (AssertionError d) {
-        System.out.println("The Adress elements dont match");
-    }
-}
+    @Test
+    @Order(value=1)
+    public void fillForm() {
+        test.navigateToPage();
+        test.fillForm();
+        
 
-@Test
-@Order(value=3)
-public void clickLenguage(){
-    test.clickLenguage();
-    try {
-        assertTrue(test.isSelected(By.id("wsf-1-field-59-row-1")));
-        System.out.println("Phyton está seleccionado.");
-    } catch (AssertionError e) {
-        System.out.println("Phyton no está seleccionado.");
-    }
-    try {
-        assertTrue(test.isSelected(By.id("wsf-1-field-59-row-2")));
-        System.out.println("PHP está seleccionado.");
-    } catch (AssertionError e) {
-        System.out.println("PHP no está seleccionado.");
-    }
-}
+        test.clickLenguage();
+        for (String language : assertLocators.keySet()) {
+            if (language.startsWith("language")) {
+                checkLanguageSelected(language);
+            }
+        }
+    
 
-@Test
-@Order(value=4)
-public void clickRadio(){
-    test.clickRadio();
+        test.clickRadio();
     try {
-        assertFalse(test.isSelected(By.xpath("//input[@id='wsf-1-field-60-row-1']")));
+        assertFalse(test.isSelected(getLocator("radio1")));
         System.out.println("Radio button 1 no está seleccionado.");
     } catch (AssertionError e) {
         System.out.println("Radio button 1 está seleccionado.");
     }
     try {
-        assertTrue(test.isSelected(By.xpath("//input[@id='wsf-1-field-60-row-2']")));
+        assertTrue(test.isSelected(getLocator("radio2")));
         System.out.println("Radio button 2 está seleccionado.");
     } catch (AssertionError e) {
         System.out.println("Radio button 2 no está seleccionado.");
     }
-}
 
-@Test
-@Order(value=5)
-public void selectSystem(){
-    test.selectSystem();
-}
-
-@Test
-@Order(value=6)
-public void clickButton(){
-    test.clickButton();
+        test.selectSystem();
+        test.clickButton();
     assertTrue(test.isDisplayed(By.id("ws-form-1")));
+
 }
+
+    private void checkInputPlaceholder(String labelKey) {
+        String inputKey = labelKey.replace("label", "input");
+        WebElement label = test.findElement(getLocator(labelKey));
+        WebElement input = test.findElement(getLocator(inputKey));
+        String labelText = label.getText();
+        String inputPlaceholder = input.getAttribute("placeholder");
+        try {
+            assertEquals(labelText, inputPlaceholder);
+        } catch (AssertionError e) {
+            System.out.println("The " + labelKey + " elements don't match");
+        }
+    }
+
+    private void checkLanguageSelected(String key) {
+        try {
+            assertTrue(test.isSelected(getLocator(key)));
+            System.out.println(key + " está seleccionado.");
+        } catch (AssertionError e) {
+            System.out.println(key + " no está seleccionado.");
+        }
+    }
 
 @AfterAll
 public static void cleanDriver(){
